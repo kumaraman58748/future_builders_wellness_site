813b855c6a61177c63a16b8be71bcad282d9c5dd
kumaraman58748 <kumaraman58748@gmail.com>
2025-11-19 14:01:31 +0530

    prebuilt frontend

--- future_builders_backend/server.js
+++ future_builders_backend/server.js
@@ -43,7 +43,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 // Catch-all route for SPA (React)
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
